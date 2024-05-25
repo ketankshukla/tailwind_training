712d3d3e5e18dbc17c2dddfedcd883491200beb5
Ketan Shukla <ketankshukla@gmail.com>
2024-05-24 19:42:22 -0700

    rounded image with margins

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,9 +5,9 @@ export function Card() {
     <main className="flex items-center justify-center bg-blue-200">
       <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-4">
         <div className="md:flex">
-          <div className="md:flex-shrink-0">
+          <div className="flex items-center justify-center md:flex-shrink-0">
             <img
-              className="h-48 w-full object-cover md:w-48"
+              className="h-48 w-full md:w-48 ml-4 rounded-full"
               src={myImage}
               alt="An image"
             />
@@ -20,7 +20,10 @@ export function Card() {
               Next.Js and React Developer
             </p>
             <p className="mt-2 text-gray-500 text-center">
-                I am a full stack developer with a focus on front end development. I have experience with React, Next.js, and Tailwind CSS. I am also familiar with Node.js, Express, and MongoDB. I am passionate about creating beautiful and functional web applications.
+              I am a full stack developer with a focus on front end development.
+              I have experience with React, Next.js, and Tailwind CSS. I am also
+              familiar with Node.js, Express, and MongoDB. I am passionate about
+              creating beautiful and functional web applications.
             </p>
           </div>
         </div>
